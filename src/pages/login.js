5029a7b6a460057dd7119ef7b062f04b6f931614
Teklehaimanot Belete <enbish.blt@gmail.com>
2021-10-27 15:10:09 +0300

    integrated login part with backend api

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,71 +1,96 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import { makeStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import axios from 'axios'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: theme.spacing(10),
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: theme.spacing(10),
 
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '300px',
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '300px',
+        },
+        '& .MuiButtonBase-root': {
+            margin: theme.spacing(2),
+        },
     },
-    '& .MuiButtonBase-root': {
-      margin: theme.spacing(2),
+    btn: {
+        color: 'green'
     },
-  },
-  btn:{
-    color: 'green'
-  },
-  cancel:{
-      color: 'red'
-  }
+    cancel: {
+        color: 'red'
+    }
 }));
 
-const Login = ({ handleClose }) => {
-  const classes = useStyles();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = () => {
+    const classes = useStyles();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const url = 'http://localhost:5000/'
+    const history = useHistory()
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log(email, password);
-    handleClose();
-  };
+    const handleSubmit = e => {
+        e.preventDefault();
 
-  return (
-    <form className={classes.root} onSubmit={handleSubmit}>
-      <TextField
-        label="Email"
-        variant="filled"
-        type="email"
-        required
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <TextField
-        label="Password"
-        variant="filled"
-        type="password"
-        required
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <div>
-        <Button variant="contained" onClick={handleClose} className = {classes.cancel}>
-          Cancel
-        </Button>
-        <Button type="submit" variant="contained" className = {classes.btn}>
-          Login
-        </Button>
-      </div>
-    </form>
-  );
+        axios.post(`${url}api/user/auth`, {
+            username: email,
+            password: password
+        })
+            .then(res => {
+                localStorage.setItem('token', res.data.token)
+                history.push('/')
+            })
+            .catch(err => {
+                console.log(err)
+                alert('incorrect password or username')
+            })
+
+
+        console.log(email, password);
+        closeOpen()
+
+    };
+
+    const closeOpen = () => {
+        setEmail('');
+        setPassword('')
+    }
+
+    return (
+        <form className={classes.root} onSubmit={handleSubmit}>
+            <TextField
+                label="Email"
+                variant="filled"
+                type="email"
+                required
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <TextField
+                label="Password"
+                variant="filled"
+                type="password"
+                required
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <div>
+                <Button variant="contained" onClick={closeOpen} className={classes.cancel}>
+                    Cancel
+                </Button>
+                <Button type="submit" variant="contained" className={classes.btn}>
+                    Login
+                </Button>
+            </div>
+        </form>
+    );
 };
 
 export default Login;
